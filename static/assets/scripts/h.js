449137be6d412186286684dac56ce5ab6bc79728
conmanclos <154739551+conmanclos@users.noreply.github.com>
2024-05-23 13:12:04 -0400

    Update h.js

--- static/assets/scripts/h.js
+++ static/assets/scripts/h.js
@@ -169,7 +169,9 @@ let SplashT = [
   "Made by zero studios",
   "Thanks for using the site",
   "play more often would ya",
-  "zero is the best",
+  "zero is the best,
+  "zero bad games",
+  "try idle tycoon start-up it is so fun",
 ]
 
 let SplashI = Math.floor(Math.random() * SplashT.length)
