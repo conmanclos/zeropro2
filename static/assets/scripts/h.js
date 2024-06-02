007e07be9037f7fa6167fb7ff2063609691b4ed6
conmanclos <154739551+conmanclos@users.noreply.github.com>
2024-06-02 10:44:31 -0500

    Update h.js

--- static/assets/scripts/h.js
+++ static/assets/scripts/h.js
@@ -6,9 +6,7 @@ try {
   inFrame = true
 }
 
-if (!inFrame && !navigator.userAgent.includes("Firefox")) {
-  const popup = open("about:blank", "_blank")
-  if (!popup || popup.closed) {
+{
     alert("Please allow popups and redirects.")
   } else {
     const doc = popup.document
